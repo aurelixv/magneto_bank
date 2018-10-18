47f3b526336157fb20badedd7837bf52972d420b
Lincoln <linkskill@gmail.com>
2018-10-18 12:18:43 -0300

    mudando sqls, adicionando mais sqls

--- queries/numTranCartao.sql
+++ queries/numTranCartao.sql
@@ -1,4 +1,5 @@
-﻿DROP VIEW IF EXISTS card1;
+﻿-- quantas transações o cliente X realizou com o cartão 1?
+/*DROP VIEW IF EXISTS card1;
 
 CREATE VIEW card1 AS (
 	SELECT ca.id, ca.client_id
@@ -13,4 +14,13 @@ SELECT count(*) AS transactions_card1, ca1.id AS card_id, ca1.client_id,
 	cl.name AS client_name
 FROM clients AS cl, transactions AS tr, card1 AS ca1
 WHERE cl.id = 686 AND cl.id = ca1.client_id AND ca1.id = tr.card_id
-GROUP BY ca1.id, ca1.client_id, cl.name;
+GROUP BY ca1.id, ca1.client_id, cl.name;*/
+
+-- Ou inserir o número do cartão e o id do cliente.
+-- QUERY: quantas transações o cliente X realizou com cartão Y?
+
+SELECT count(*) AS num_transactions, ca.id AS card_id, ca.client_id, 
+	cl.name AS client_name
+FROM clients AS cl, transactions AS tr, cards AS ca
+WHERE cl.id = 'PARAM' AND ca.card_number = 'PARAM' AND cl.id = ca.client_id AND ca. AND ca.id = tr.card_id
+GROUP BY ca.id, ca.client_id, cl.name;
--- queries/saldoAnual.sql
+++ queries/saldoAnual.sql
@@ -1,3 +1,6 @@
-﻿SELECT SUM(value)
+﻿-- Passar o ano e o cartão por parâmetro
+-- QUERY: qual é o saldo de um determinado cliente no ano tal?
+
+SELECT SUM(value)
 FROM transactions
-WHERE card_id = 1039 AND EXTRACT(YEAR FROM transaction_date) = 2017;
+WHERE card_id = 1039 AND EXTRACT(YEAR FROM transaction_date) = 'PARAM';
